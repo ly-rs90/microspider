0a47cdfa89def5289e9214c664db413bd675a456
LY <ly@LYdeMacBook-Pro.local>
2020-02-22 23:41:38 +0800

    并发量改用Semaphore控制，随机生成浏览器标识。

--- spider/spider.py
+++ spider/spider.py
@@ -20,6 +20,7 @@ from asyncio.queues import Queue
 from ..log.logger import Logger
 from ..web.url import URL
 from ..web.response import Response
+from ..web.agent import UserAgent
 
 __all__ = ['Spider']
 
@@ -27,42 +28,63 @@ __all__ = ['Spider']
 class Spider:
     """web爬虫"""
 
-    # 总并发请求数
-    MAX_REQUEST_TOTAL = 20
+    # 总工作者数量
+    MAX_WORKER = 20
 
-    # 单个域名并发请求数
-    MAX_REQUEST_DOMAIN = 5
+    # 单个域名最大工作者数量
+    WORKER_DOMAIN = 5
 
     # 允许的域名
     ALLOWED_DOMAIN = []
 
     def __init__(self):
         # 具体任务队列，每个key(域名)对应一个队列
-        self._task = dict()
+        self._url_queue = dict()
 
-        # 每个域名对应的空位
-        self._free_domain = dict()
+        # 总的工位，每一个worker工作时需要先获取一个工位
+        self._work_position = None
 
-        # 总的空闲队列
-        self._free_queue = None
+        # 每个域名对应的工作者
+        self._worker = dict()
 
         # 已经处理的url
         self._handled_urls = set()
 
-        # 当前正在处理的url
-        self._handling = []
-
-        # 正在运行的任务
-        self._running_task = dict()
+        # 正在运行的爬取任务
+        self._running_task = None
 
         # 时间基准
         self._base_time = time.time()
 
         # 完成的任务数
         self._completed = 0
+    
+    async def add_task(self, *urls):
+        """添加新任务"""
+        
+        for url in urls:
+            # 域名不在允许列表内
+            if not self._check_domain(url):
+                continue
 
-        # 任务完成事件
-        self._task_done = None
+            hash_value = URL.md5_value(url)
+            # 重复url
+            if hash_value in self._handled_urls:
+                continue
+
+            self._handled_urls.add(hash_value)
+            u = URL(url)
+            if self._url_queue.get(u.host):
+                await self._url_queue[u.host].put(url)
+            else:
+                # 为新的域名创建url队列
+                self._url_queue[u.host] = Queue()
+                await self._url_queue[u.host].put(url)
+
+                # 初始化工作者
+                self._worker[u.host] = asyncio.Semaphore(self.WORKER_DOMAIN)
+                # 为新的域名创建一个调度任务
+                asyncio.create_task(self._task_monitor(self._url_queue[u.host], self._worker[u.host]))
     
     async def handle_document(self, response):
         """处理http响应
@@ -72,62 +94,7 @@ class Spider:
         self.add_task(*urls)      # 继续爬取新的链接
         """
         pass
-
-    async def _init(self):
-        """初始化"""
-        loop = asyncio.get_running_loop()
-        self._free_queue = Queue(self.MAX_REQUEST_TOTAL)
-        self._task_done = loop.create_future()
-
-        # 初始化空闲队列
-        for _ in range(self.MAX_REQUEST_TOTAL):
-            await self._free_queue.put(1)
-        
-        asyncio.create_task(self._record())
-
-    async def _task_schedule(self, *url):
-
-        try:
-            await self._init()
-            await self.add_task(*url)
-            await self._task_done
-
-            avg = self._completed / (time.time() - self._base_time) * 60
-            Logger.log(f'任务完成，共爬取资源：{self._completed}，平均速度：{round(avg, 2)}/min。耗时：{round(time.time()-self._base_time, 3)}s.')
-            return True
-        except:
-            return False
     
-    async def _schedule(self, task_queue, position_queue):
-        """执行任务调度"""
-
-        while True:
-            url = await task_queue.get()
-            self._handling.append(url)
-            await position_queue.get()
-            await self._free_queue.get()
-            task = asyncio.create_task(self._crawl_document(url))
-            self._running_task[url] = task
-            self._free_queue.task_done()
-            position_queue.task_done()
-            task_queue.task_done()
-
-    def _gzip_decompress(self, data):
-        """gzip解压缩"""
-
-        try:
-            return gzip.decompress(data)
-        except:
-            return None
-    
-    def _deflate_decompress(self, data):
-        """deflate解压"""
-
-        try:
-            return zlib.decompress(data, -zlib.MAX_WBITS)
-        except:
-            return None
-
     def _check_domain(self, url):
         """检查域名是否在允许的列表内"""
 
@@ -140,8 +107,28 @@ class Spider:
                 return True
         return False
 
+    async def _commit_task(self):
+        """确认运行的任务"""
+        
+        while True:
+            task = await self._running_task.get()
+
+            # 等待任务完成
+            if not task.done():
+                await task
+            
+            self._running_task.task_done()
+    
+    def _deflate_decompress(self, data):
+        """deflate解压"""
+
+        try:
+            return zlib.decompress(data, -zlib.MAX_WBITS)
+        except:
+            return None
+
     async def _get_connector(self, url):
-        """连接web主机"""
+        """获取连接到web主机的传输对象"""
 
         u = URL(url)
         data = None
@@ -158,32 +145,12 @@ class Spider:
         except:
             return None, None
     
-    async def add_task(self, *urls):
-        """添加新任务"""
-        
-        # 筛选url添加到任务队列
-        for url in urls:
-            if self._check_domain(url):
-                hash_value = URL.md5_value(url)
-                if hash_value not in self._handled_urls:
-                    self._handled_urls.add(hash_value)
-                    u = URL(url)
-                    if self._free_domain.get(u.host):
-                        await self._task[u.host].put(url)
-                    else:
-                        self._free_domain[u.host] = Queue(self.MAX_REQUEST_DOMAIN)
-                        for _ in range(self.MAX_REQUEST_DOMAIN):
-                            await self._free_domain[u.host].put(1)
+    def _get_request_header(self, url, method='GET'):
+        """根据url生成对应的HTTP请求头"""
 
-                        self._task[u.host] = Queue()
-                        await self._task[u.host].put(url)
-                        asyncio.create_task(self._schedule(self._task[u.host], self._free_domain[u.host]))
-    
-    def _get_header(self, url, **param):
         u = URL(url)
 
-        query_user_gen = (f'{key}={value}' for key, value in param.items())
-        query = u.query + '&'.join(query_user_gen)
+        query = u.query
         if query:
             query += '?'
 
@@ -194,7 +161,7 @@ class Spider:
             'Pragma: no-cache\r\n'
             'Cache-Control: no-cache\r\n'
             'Upgrade-Insecure-Requests: 1\r\n'
-            'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36\r\n'
+            f'User-Agent: {UserAgent.get_agent()}\r\n'
             'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n'
             'Accept-Language: zh-CN,zh;q=0.9\r\n'
             'Accept-Encoding: gzip, deflate\r\n'
@@ -203,20 +170,18 @@ class Spider:
 
         return h
     
-    async def _record(self):
-        """状态报告"""
+    async def _get_response_header(self, reader, url):
+        """获取HTTP响应头"""
 
-        while True:
-            await asyncio.sleep(60)
-
-            now = time.time()
-            task_num = 0
-            for key, v in self._task.items():
-                task_num += v.qsize()
-            Logger.log(f'当前任务量：{task_num}。成功爬取文档：{self._completed}，平均速度：{round(self._completed/(now-self._base_time)*60, 2)}/min')
+        try:
+            header = await reader.readuntil(b'\r\n\r\n')
+            return header
+        except Exception as e:
+            Logger.log(f'(read header error) GET {url}\n详情：{e}')
+            return b''
     
-    async def _read_chunk_data(self, reader):
-        """读取分块传输的内容"""
+    async def _get_chunk_data(self, reader, url):
+        """获取分块传输的内容"""
 
         content = b''
 
@@ -228,96 +193,161 @@ class Spider:
                 content += chunk[:-2]
                 chunk_line = await reader.readline()
         except Exception as e:
-            self._log(f'获取分块传输的内容失败：{e}')
+            content = b''
+            self._log(f'(read chunk body error) GET {url}\n详情：{e}')
 
         return content
-    
-    async def _check_task_done(self):
-        """检查任务是否完成"""
 
-        if self._free_queue.qsize() == self.MAX_REQUEST_TOTAL:
-            for _, value in self._task.items():
-                if value.empty():
-                    continue
-                else:
-                    break
-            else:
-                self._task_done.set_result(1)
-    
-    async def _crawl_document(self, url):
-        """爬取文档"""
-
-        u = URL(url)
-        reader, writer = await self._get_connector(url)
-        if not reader or not writer:
-            Logger.log(f'GET {url}失败，无法连接主机！')
-            await self._free_domain[u.host].put(1)
-            await self._free_queue.put(1)
-            # 检查任务是否完成
-            await self._check_task_done()
-            try:
-                self._handling.remove(url)
-                self._running_task.pop(url)
-            except:
-                pass
-            return
+    async def _get_body(self, reader, length, url):
+        """获取length长度的响应体"""
 
         try:
-            writer.write(self._get_header(url).encode())
-            await writer.drain()
-
-            response_header = await reader.readuntil(b'\r\n\r\n')
-            if not response_header:
-                Logger.log(f'GET {url}失败，连接已断开！')
-                await self._free_domain[u.host].put(1)
-                await self._free_queue.put(1)
-                writer.close()
-                await writer.wait_closed()
-                # 检查任务是否完成
-                await self._check_task_done()
-                try:
-                    self._handling.remove(url)
-                    self._running_task.pop(url)
-                except:
-                    pass
-                return
-
-            response = Response(url, response_header)
-            # 传输编码
-            transfer_encoding = response.get('Transfer-Encoding', '')
-            if transfer_encoding == 'chunked':
-                body = await self._read_chunk_data(reader)
-            else:
-                content_length = int(response.get('Content-Length', '0'))
-                body = await reader.readexactly(content_length)
-            
-            # 内容编码
-            content_encoding = response.get('Content-Encoding', '')
-            if content_encoding == 'gzip':
-                body = self._gzip_decompress(body)
-            elif content_encoding == 'deflate':
-                body = self._deflate_decompress(body)
-
-            response.set_body(body)
-            Logger.log(f'({response.code}) GET {url}')
-            self._completed += 1
-            await self.handle_document(response)
+            data = await reader.readexactly(length)
+            return data
         except Exception as e:
-            Logger.log(f'未知错误：{e}')
-        finally:
-            await self._free_domain[u.host].put(1)
-            await self._free_queue.put(1)
+            Logger.log(f'(read body error) GET {url}\n详情：{e}')
+            return b''
+    
+    def _gzip_decompress(self, data):
+        """gzip解压"""
+
+        try:
+            return gzip.decompress(data)
+        except:
+            return None
+    
+    async def _record(self):
+        """状态报告"""
+
+        while True:
+            await asyncio.sleep(60)
+
+            now = time.time()
+            task_num = 0
+            for key, v in self._url_queue.items():
+                task_num += v.qsize()
+            Logger.log(f'当前任务量：{task_num}。成功爬取文档：{self._completed}，平均速度：{round(self._completed/(now-self._base_time)*60, 2)}/min ')
+    
+    async def _send_request_header(self, writer, url):
+        """发送HTTP请求头"""
+
+        try:
+            header = self._get_request_header(url)
+            writer.write(header.encode())
+            await writer.drain()
+            return True
+        except Exception as e:
+            Logger.log(f'(send header error) {url}\n详情：{e}')
+            return False
+    
+    def _task_done(self, url):
+        """对应的url处理完成"""
+
+        u = URL(url)
+        # 释放工作者
+        self._worker[u.host].release()
+        # 释放工位
+        self._work_position.release()
+
+    async def _init(self):
+        """初始化"""
+        
+        self._work_position = asyncio.Semaphore(self.MAX_WORKER)
+        self._running_task = Queue(self.MAX_WORKER)
+        
+        # 创建报告任务，每隔1分钟报告一次当前爬取状态
+        asyncio.create_task(self._record())
+        # 创建任务检查
+        asyncio.create_task(self._commit_task())
+
+    async def _run(self, *url):
+        """开始任务，直到任务完成"""
+
+        try:
+            await self._init()
+            await self.add_task(*url)
+            await asyncio.sleep(1)
+            await self._running_task.join()
+
+            avg = self._completed / (time.time() - self._base_time) * 60
+            Logger.log(f'任务完成，共爬取资源：{self._completed}，平均速度：{round(avg, 2)}/min。耗时：{round(time.time()-self._base_time, 3)}s.')
+            return True
+        except:
+            return False
+    
+    async def _task_monitor(self, url_queue, worker):
+        """执行任务调度"""
+
+        while True:
+            url = await url_queue.get()
+            # 等待工作者
+            await worker.acquire()
+            # 等待工位
+            await self._work_position.acquire()
+            # 创建爬取任务
+            task = asyncio.create_task(self._download_document(url))
+            # 将任务加到运行队列
+            await self._running_task.put(task)
+    
+    async def _download_document(self, url):
+        """下载文档"""
+
+        reader, writer = await self._get_connector(url)
+        if not reader or not writer:
+            Logger.log(f'(connect error) GET {url}')
+            self._task_done(url)
+            return
+
+        flag = await self._send_request_header(writer, url)
+        if not flag:
+            self._task_done(url)
             writer.close()
             await writer.wait_closed()
-            # 检查任务是否完成
-            await self._check_task_done()
-            try:
-                self._handling.remove(url)
-                self._running_task.pop(url)
-            except:
-                pass
+            return
+        
+        response_header = await self._get_response_header(reader, url)
+        if not response_header:
+            self._task_done(url)
+            writer.close()
+            await writer.wait_closed()
+            return
+        
+        response = Response(url, response_header)
+        # 传输编码
+        transfer_encoding = response.get('Transfer-Encoding', '')
+        if transfer_encoding == 'chunked':
+            body = await self._get_chunk_data(reader, url)
+        else:
+            content_length = int(response.get('Content-Length', '0'))
+            body = await self._get_body(reader, content_length, url)
+        
+        # 数据读取完成，关闭连接
+        writer.close()
+        await writer.wait_closed()
+        
+        if not body:
+            self._task_done(url)
+            return
+        
+        # 内容编码
+        content_encoding = response.get('Content-Encoding', '')
+        if content_encoding == 'gzip':
+            body = self._gzip_decompress(body)
+        elif content_encoding == 'deflate':
+            body = self._deflate_decompress(body)
+        
+        response.set_body(body)
+        Logger.log(f'({response.code}) GET {url}')
+        self._completed += 1
+
+        try:
+            await self.handle_document(response)
+        except Exception as e:
+            Logger.log(f'处理响应出现错误：{e}')
+        
+        self._task_done(url)
     
     def start(self, *urls):
         """开始爬取"""
         
-        asyncio.run(self._task_schedule(*urls))
+        asyncio.run(self._run(*urls))
